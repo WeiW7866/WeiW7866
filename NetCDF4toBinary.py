d641846e98c08da92eb8c9d51036a3dfa4f0531f
WeiW7866 <165594972+WeiW7866@users.noreply.github.com>
2024-04-06 00:30:49 -0400

    Update and rename NPPCloud.py to NetCDF4toBinary.py

--- NetCDF4toBinary.py
+++ NetCDF4toBinary.py
@@ -1,7 +1,6 @@
-# Suomi National Polar-orbiting Partnership(S-NPP)
-# Save cloud data as .dat format
-# This is created by Wei Wang
-# Contact me via :wangwei20160708@gmail.com
+# Convert NetCDF4 to Binary format
+# Developed by Wei Wang on 04/02/2022
+# wangwei20160708@gmail.com
 
 import datetime as dt                                               # Python standard library datetime  module
 import numpy as np
@@ -10,12 +9,12 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap, addcyclic, shiftgrid
 import os.path
 
-inputfn_dir = 'xxxxxxxxxxxx'
-outputfn_dir = 'xxxxxxxxxxxx'
+inputfn_dir = 'This is a input directory'
+outputfn_dir = 'This is a output directory'
 
 #count = 0
 
-for year in range(20xx, 20xx):
+for year in range(2012, 2020):
     year_str = f"{year:04}"
     print(year_str)
     for month in range(10, 11):
@@ -38,11 +37,11 @@ for year in range(20xx, 20xx):
                     full_output_path = outputfn_dir + '/' + foutn
                     if os.path.exists(ncfl):
                         nc_fid = Dataset(ncfl, 'r')
-                        lats = nc_fid.variables['Latitude'][:]
-                        lons = nc_fid.variables['Longitude'][:]
-                        sst = nc_fid.variables['CloudProbability'][:][:]
-                        dat = np.array([lats, lons, sst])
-                        print(sst.shape)
+                        lats = nc_fid.variables['Latitude'][:]                                                          # Attribute Latitude
+                        lons = nc_fid.variables['Longitude'][:]                                                         # Attribute Longitude
+                        cpb = nc_fid.variables['CloudProbability'][:][:]                                                # Attribute Cloud Probability, please replace it for your purpose
+                        dat = np.array([lats, lons, cpb])
+                        print(cpb.shape)
                         
                         f=open(full_output_path,"wb")
                         f.write(dat)
